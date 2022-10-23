fb853d59c61a3458d4b9f88c205d2d2007445ee2
user_name <12120524+dummyindex@users.noreply.github.com>
2022-10-23 10:57:49 -0700

    Update run_exp.sh

--- F22_10703_Homework_2/hw2_impl/pytorch/a2c/run_exp.sh
+++ F22_10703_Homework_2/hw2_impl/pytorch/a2c/run_exp.sh
@@ -1,1 +1,6 @@
 python run.py --net-type=Reinforce
+python run.py --net-type=Baseline
+python run.py --net-type=A2C
+python run.py --net-type=A2C --n=1
+python run.py --net-type=A2C --n=10
+python run.py --net-type=A2C --n=100
